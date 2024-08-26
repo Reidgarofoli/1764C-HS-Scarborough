5d1c8e1aa73c80bc20ff9cd2d17a24b6e3f89162
Reidgarofoli <136121523+Reidgarofoli@users.noreply.github.com>
2024-08-26 11:28:04 +0000

    dfs

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,20 @@ pros::Motor LeftBack(-6, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_
 pros::Motor ptoR(7, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor ptoL(8, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
 
+pros::Motor IntakeStageOne(20, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor IntakeStageTwo(19, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+
+#define ptopin 4 // D
+pros::ADIDigitalOut pto (ptopin-1);
+bool ptovalue = false;
+
+#define mogo 5 // E
+pros::ADIDigitalOut mogomech (mogo);
+pros::ADIDigitalOut mogomechtwo (mogo-1);
+bool mogovalue = false;
+
 char ptoShift = 'L'; // L=lift    D=drive
-/*
+
 		
 //Drivetrain config 
 lemlib::Drivetrain drivetrain {
@@ -89,19 +101,19 @@ lemlib::ControllerSettings angularController {
 
 // create the chassis
 lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
-*/
+
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "code works");
 
-	//chassis.calibrate();
+	chassis.calibrate();
 
 	// set position to x:0, y:0, heading:0
-    //chassis.setPose(0, 0, 0);
-    // turn to face heading 90 with a very long timeout
-    //chassis.turnTo(-24, 0, 10000000);
-	//chassis.moveToPoint(-24, 0, 10000000);
-	//chassis.turnTo(-24, 24, 10000000);
+    // chassis.setPose(0, 0, 0);
+    // chassis.moveToPoint(0, -30, 100000000, false);
+    // chassis.moveToPoint(10, -45, 100000000, false);
+	// chassis.turnTo(10,-24, 100000);
+	// chassis.moveToPoint(20, 0, 100000);
 }
 
 
@@ -116,14 +128,19 @@ void autonomous() {}
 void shiftPTO(){
 	if (ptoShift == 'L'){
 		ptoShift = 'D';
+		pto.set_value(false);
 	} else {
 		ptoShift = 'L';
+		pto.set_value(true);
 	}
 }
 
 void opcontrol() {
 
-
+	mogomech.set_value(mogovalue);
+	mogomechtwo.set_value(mogovalue);
+	shiftPTO();// There has to be two DONT DELETE
+	shiftPTO();// There has to be two DONT DELETE
 	while (true) {
 		int dir = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
 		int turn = -controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
@@ -136,6 +153,30 @@ void opcontrol() {
 		}
 		if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_A)){
 			shiftPTO();
+		}
+		if (controller.get_digital(E_CONTROLLER_DIGITAL_R1)){
+			IntakeStageOne.move(127);
+		} else if (controller.get_digital(E_CONTROLLER_DIGITAL_R2)){
+			IntakeStageOne.move(-127);
+		} else {
+			IntakeStageOne.move(0);
+		}
+
+		if (controller.get_digital(E_CONTROLLER_DIGITAL_L1) && ptoShift == 'L'){
+			ptoL.move(127);
+			ptoR.move(127);
+		} else if (controller.get_digital(E_CONTROLLER_DIGITAL_L2) && ptoShift == 'L'){
+			ptoL.move(-127);
+			ptoR.move(-127);
+		} else if (ptoShift == 'L') {
+			ptoL.move(0);
+			ptoR.move(0);
+		}
+
+		if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_DOWN)){
+			mogomech.set_value(!mogovalue);
+			mogomechtwo.set_value(!mogovalue);
+			mogovalue = !mogovalue;
 		}
 		pros::delay(20);
 	}
